29db5c5cea6ec0c32ee223ba4eadc389c2886ede
nirajmistry111 <105445204+nirajmistry111@users.noreply.github.com>
2022-06-03 23:32:01 -0400

    Add files via upload

---
+++ ICE2_DiseaseCases/Form1.Designer.cs
@@ -0,0 +1,336 @@
+﻿
+namespace ICE2_DiseaseCases
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblWeekDays = new System.Windows.Forms.Label();
+            this.lblDay1 = new System.Windows.Forms.Label();
+            this.lblDay2 = new System.Windows.Forms.Label();
+            this.lblDay3 = new System.Windows.Forms.Label();
+            this.lblDay4 = new System.Windows.Forms.Label();
+            this.lblDay5 = new System.Windows.Forms.Label();
+            this.lblDay6 = new System.Windows.Forms.Label();
+            this.lblDay7 = new System.Windows.Forms.Label();
+            this.txtDay1 = new System.Windows.Forms.TextBox();
+            this.txtDay2 = new System.Windows.Forms.TextBox();
+            this.txtDay3 = new System.Windows.Forms.TextBox();
+            this.txtDay4 = new System.Windows.Forms.TextBox();
+            this.txtDay5 = new System.Windows.Forms.TextBox();
+            this.txtDay6 = new System.Windows.Forms.TextBox();
+            this.txtDay7 = new System.Windows.Forms.TextBox();
+            this.lblInfectionsTitle = new System.Windows.Forms.Label();
+            this.dtpStartingDate = new System.Windows.Forms.DateTimePicker();
+            this.lblStartingDate = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblDailyAverageLabel = new System.Windows.Forms.Label();
+            this.lblDailyAverage = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblWeekDays
+            // 
+            this.lblWeekDays.AutoSize = true;
+            this.lblWeekDays.Location = new System.Drawing.Point(90, 132);
+            this.lblWeekDays.Name = "lblWeekDays";
+            this.lblWeekDays.Size = new System.Drawing.Size(75, 20);
+            this.lblWeekDays.TabIndex = 0;
+            this.lblWeekDays.Text = "Weekdays";
+            // 
+            // lblDay1
+            // 
+            this.lblDay1.AutoSize = true;
+            this.lblDay1.Location = new System.Drawing.Point(90, 175);
+            this.lblDay1.Name = "lblDay1";
+            this.lblDay1.Size = new System.Drawing.Size(50, 20);
+            this.lblDay1.TabIndex = 1;
+            this.lblDay1.Text = "label1";
+            // 
+            // lblDay2
+            // 
+            this.lblDay2.AutoSize = true;
+            this.lblDay2.Location = new System.Drawing.Point(90, 215);
+            this.lblDay2.Name = "lblDay2";
+            this.lblDay2.Size = new System.Drawing.Size(50, 20);
+            this.lblDay2.TabIndex = 2;
+            this.lblDay2.Text = "label2";
+            // 
+            // lblDay3
+            // 
+            this.lblDay3.AutoSize = true;
+            this.lblDay3.Location = new System.Drawing.Point(90, 255);
+            this.lblDay3.Name = "lblDay3";
+            this.lblDay3.Size = new System.Drawing.Size(50, 20);
+            this.lblDay3.TabIndex = 3;
+            this.lblDay3.Text = "label3";
+            // 
+            // lblDay4
+            // 
+            this.lblDay4.AutoSize = true;
+            this.lblDay4.Location = new System.Drawing.Point(90, 295);
+            this.lblDay4.Name = "lblDay4";
+            this.lblDay4.Size = new System.Drawing.Size(50, 20);
+            this.lblDay4.TabIndex = 4;
+            this.lblDay4.Text = "label4";
+            // 
+            // lblDay5
+            // 
+            this.lblDay5.AutoSize = true;
+            this.lblDay5.Location = new System.Drawing.Point(90, 339);
+            this.lblDay5.Name = "lblDay5";
+            this.lblDay5.Size = new System.Drawing.Size(50, 20);
+            this.lblDay5.TabIndex = 5;
+            this.lblDay5.Text = "label5";
+            // 
+            // lblDay6
+            // 
+            this.lblDay6.AutoSize = true;
+            this.lblDay6.Location = new System.Drawing.Point(90, 379);
+            this.lblDay6.Name = "lblDay6";
+            this.lblDay6.Size = new System.Drawing.Size(50, 20);
+            this.lblDay6.TabIndex = 6;
+            this.lblDay6.Text = "label6";
+            // 
+            // lblDay7
+            // 
+            this.lblDay7.AutoSize = true;
+            this.lblDay7.Location = new System.Drawing.Point(90, 419);
+            this.lblDay7.Name = "lblDay7";
+            this.lblDay7.Size = new System.Drawing.Size(50, 20);
+            this.lblDay7.TabIndex = 7;
+            this.lblDay7.Text = "label7";
+            // 
+            // txtDay1
+            // 
+            this.txtDay1.Location = new System.Drawing.Point(195, 171);
+            this.txtDay1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtDay1.MaxLength = 4;
+            this.txtDay1.Name = "txtDay1";
+            this.txtDay1.Size = new System.Drawing.Size(67, 27);
+            this.txtDay1.TabIndex = 8;
+            // 
+            // txtDay2
+            // 
+            this.txtDay2.Location = new System.Drawing.Point(195, 211);
+            this.txtDay2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtDay2.MaxLength = 4;
+            this.txtDay2.Name = "txtDay2";
+            this.txtDay2.Size = new System.Drawing.Size(67, 27);
+            this.txtDay2.TabIndex = 9;
+            // 
+            // txtDay3
+            // 
+            this.txtDay3.Location = new System.Drawing.Point(195, 251);
+            this.txtDay3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtDay3.MaxLength = 4;
+            this.txtDay3.Name = "txtDay3";
+            this.txtDay3.Size = new System.Drawing.Size(67, 27);
+            this.txtDay3.TabIndex = 10;
+            // 
+            // txtDay4
+            // 
+            this.txtDay4.Location = new System.Drawing.Point(195, 291);
+            this.txtDay4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtDay4.MaxLength = 4;
+            this.txtDay4.Name = "txtDay4";
+            this.txtDay4.Size = new System.Drawing.Size(67, 27);
+            this.txtDay4.TabIndex = 11;
+            // 
+            // txtDay5
+            // 
+            this.txtDay5.Location = new System.Drawing.Point(195, 335);
+            this.txtDay5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtDay5.MaxLength = 4;
+            this.txtDay5.Name = "txtDay5";
+            this.txtDay5.Size = new System.Drawing.Size(67, 27);
+            this.txtDay5.TabIndex = 12;
+            // 
+            // txtDay6
+            // 
+            this.txtDay6.Location = new System.Drawing.Point(195, 375);
+            this.txtDay6.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtDay6.MaxLength = 4;
+            this.txtDay6.Name = "txtDay6";
+            this.txtDay6.Size = new System.Drawing.Size(67, 27);
+            this.txtDay6.TabIndex = 13;
+            // 
+            // txtDay7
+            // 
+            this.txtDay7.Location = new System.Drawing.Point(195, 415);
+            this.txtDay7.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtDay7.MaxLength = 4;
+            this.txtDay7.Name = "txtDay7";
+            this.txtDay7.Size = new System.Drawing.Size(67, 27);
+            this.txtDay7.TabIndex = 14;
+            // 
+            // lblInfectionsTitle
+            // 
+            this.lblInfectionsTitle.AutoSize = true;
+            this.lblInfectionsTitle.Location = new System.Drawing.Point(195, 132);
+            this.lblInfectionsTitle.Name = "lblInfectionsTitle";
+            this.lblInfectionsTitle.Size = new System.Drawing.Size(73, 20);
+            this.lblInfectionsTitle.TabIndex = 15;
+            this.lblInfectionsTitle.Text = "Infections";
+            // 
+            // dtpStartingDate
+            // 
+            this.dtpStartingDate.Location = new System.Drawing.Point(90, 67);
+            this.dtpStartingDate.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dtpStartingDate.Name = "dtpStartingDate";
+            this.dtpStartingDate.Size = new System.Drawing.Size(228, 27);
+            this.dtpStartingDate.TabIndex = 16;
+            // 
+            // lblStartingDate
+            // 
+            this.lblStartingDate.AutoSize = true;
+            this.lblStartingDate.Location = new System.Drawing.Point(90, 39);
+            this.lblStartingDate.Name = "lblStartingDate";
+            this.lblStartingDate.Size = new System.Drawing.Size(97, 20);
+            this.lblStartingDate.TabIndex = 17;
+            this.lblStartingDate.Text = "Starting Date";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(90, 520);
+            this.btnCalculate.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(86, 64);
+            this.btnCalculate.TabIndex = 18;
+            this.btnCalculate.Text = "&Calculate Average";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblDailyAverageLabel
+            // 
+            this.lblDailyAverageLabel.AutoSize = true;
+            this.lblDailyAverageLabel.Location = new System.Drawing.Point(90, 472);
+            this.lblDailyAverageLabel.Name = "lblDailyAverageLabel";
+            this.lblDailyAverageLabel.Size = new System.Drawing.Size(102, 20);
+            this.lblDailyAverageLabel.TabIndex = 19;
+            this.lblDailyAverageLabel.Text = "Daily Average";
+            // 
+            // lblDailyAverage
+            // 
+            this.lblDailyAverage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblDailyAverage.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblDailyAverage.Location = new System.Drawing.Point(195, 464);
+            this.lblDailyAverage.Name = "lblDailyAverage";
+            this.lblDailyAverage.Size = new System.Drawing.Size(67, 39);
+            this.lblDailyAverage.TabIndex = 20;
+            this.lblDailyAverage.Text = "0";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(183, 520);
+            this.btnReset.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(86, 64);
+            this.btnReset.TabIndex = 21;
+            this.btnReset.Text = "&Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(275, 520);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(86, 64);
+            this.btnExit.TabIndex = 22;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(438, 600);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.lblDailyAverage);
+            this.Controls.Add(this.lblDailyAverageLabel);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblStartingDate);
+            this.Controls.Add(this.dtpStartingDate);
+            this.Controls.Add(this.lblInfectionsTitle);
+            this.Controls.Add(this.txtDay7);
+            this.Controls.Add(this.txtDay6);
+            this.Controls.Add(this.txtDay5);
+            this.Controls.Add(this.txtDay4);
+            this.Controls.Add(this.txtDay3);
+            this.Controls.Add(this.txtDay2);
+            this.Controls.Add(this.txtDay1);
+            this.Controls.Add(this.lblDay7);
+            this.Controls.Add(this.lblDay6);
+            this.Controls.Add(this.lblDay5);
+            this.Controls.Add(this.lblDay4);
+            this.Controls.Add(this.lblDay3);
+            this.Controls.Add(this.lblDay2);
+            this.Controls.Add(this.lblDay1);
+            this.Controls.Add(this.lblWeekDays);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblWeekDays;
+        private System.Windows.Forms.Label lblDay1;
+        private System.Windows.Forms.Label lblDay2;
+        private System.Windows.Forms.Label lblDay3;
+        private System.Windows.Forms.Label lblDay4;
+        private System.Windows.Forms.Label lblDay5;
+        private System.Windows.Forms.Label lblDay6;
+        private System.Windows.Forms.Label lblDay7;
+        private System.Windows.Forms.TextBox txtDay1;
+        private System.Windows.Forms.TextBox txtDay2;
+        private System.Windows.Forms.TextBox txtDay3;
+        private System.Windows.Forms.TextBox txtDay4;
+        private System.Windows.Forms.TextBox txtDay5;
+        private System.Windows.Forms.TextBox txtDay6;
+        private System.Windows.Forms.TextBox txtDay7;
+        private System.Windows.Forms.Label lblInfectionsTitle;
+        private System.Windows.Forms.DateTimePicker dtpStartingDate;
+        private System.Windows.Forms.Label lblStartingDate;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblDailyAverageLabel;
+        private System.Windows.Forms.Label lblDailyAverage;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ ICE2_DiseaseCases/Form1.cs
@@ -0,0 +1,132 @@
+﻿// Name: - Niraj Mistry.
+// Student no: -100855211    
+//Date: - 02/06/2022.
+// Disease Cases average calculator.
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ICE2_DiseaseCases
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #region variables
+        // Global Variables
+        Label[] weekDays;
+        TextBox[] infections;
+        double totalcases = 0;
+        
+        // TODO: Add an event (ValueChanged) to the DateTimePicker control such that the weekday names are updated to match the chosen date. Note: the custom method to enter the names is already created, just call it.
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // initialize the weekdays label array and populate with the appropriate labels
+            weekDays = new Label[] { lblDay1, lblDay2, lblDay3, lblDay4, lblDay5, lblDay6, lblDay7 };
+
+
+            // TODO: initialize the infections textbox array and populate with the appropriate textboxes
+            infections = new TextBox[] { txtDay1, txtDay2, txtDay3, txtDay4, txtDay5, txtDay6, txtDay7 };
+           
+            
+            // populate the week day name labels with the actual names.
+            SetDayNames();
+        }
+
+
+        private void SetDayNames()
+        {
+            for (int day = 1; day <= 7; day++)
+            {
+                //TODO: review the below line to see if you can figure out how it works!
+                weekDays[day - 1].Text = (dtpStartingDate.Value.AddDays(day - 1)).DayOfWeek.ToString();
+            }
+        }
+        #endregion
+        #region method
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            double averagecases = 0;
+            int validateInfections = 0;
+
+            // this will check each and every element of the array. 
+            foreach (TextBox textBoxToCheck in infections)
+            {
+                // If the textbox is valid, count it. If not, just don't.
+                if (ValidateInfections(textBoxToCheck))
+                {
+                    validateInfections++;
+                }
+            }
+
+            if (validateInfections == infections.Length)
+            {
+                //formula to calculate the average .
+                averagecases = Math.Round(totalcases / infections.Length, 2);
+
+                // this will print the average in the lable of the output.
+                lblDailyAverage.Text = averagecases.ToString();
+
+            }
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            // this is the code to exit the application.
+            Application.Exit();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            // this will reset the form to the initial stage.
+            txtDay1.Clear();
+            txtDay2.Clear();
+            txtDay3.Clear();
+            txtDay4.Clear();
+            txtDay5.Clear();
+            txtDay6.Clear();
+            txtDay7.Clear();
+            lblDailyAverage.ResetText();
+            totalcases = 0;
+        }
+        #endregion
+        #region functions
+
+        private bool ValidateInfections(TextBox textBoxInput)
+        {
+
+            // declaring thre variables.
+            double inputcases;
+            bool isValid = false;
+
+            //TODO: complete this function such that if any of the infection textboxes are not numeric then validation fails.  It is expected to do this in a modular way (i.e. looping, rather than using the textbox names.)
+            if (double.TryParse(textBoxInput.Text, out inputcases))
+            {
+                
+                isValid = true;
+                totalcases += inputcases;
+            }
+            else
+            {
+                
+
+                lblDailyAverage.Text += "The entered value of '" + textBoxInput.Text + "' is not valid. Please enter a numeric temperature.\r\n";
+
+            }
+            return isValid;
+        }
+        #endregion 
+    }
+}
